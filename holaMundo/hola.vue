5747df6a5982480303f0371bb9b6e91ea2f3779d
isaihdz <iasiher13@gmail.com>
2022-09-11 18:32:14 -0500

    first example, check the error

---
+++ holaMundo/hola.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Lorem ipsum </p>
+    <p>{{ text }} </p>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        text: "Hola"
+      };
+    }
+  };
+</script>
+
+<style>
+  p{
+    color: papayawhip;
+  }
+</style>
